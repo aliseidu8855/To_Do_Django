10f6cb72fe3cddfd0c7171ddb8b236a0c210b569
aliseidu8855 <aliseidu8855@gmail.com>
2024-11-28 10:51:09 +0000

    Implement form submission for to-do items and update tests

--- lists/views.py
+++ lists/views.py
@@ -4,4 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home_page(request):
-    return render(request, "home.html")
+    return render(request, 
+                  "home.html", 
+                  {"new_item_text": request.post.get("item_text", "")},)
