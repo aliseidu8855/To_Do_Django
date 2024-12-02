cfb68bbccbde1d166ffae00ba1462888a8f5b37e
aliseidu8855 <aliseidu8855@gmail.com>
2024-12-02 19:58:21 +0000

    Add item numbering to home page and fix POST request handling

--- lists/views.py
+++ lists/views.py
@@ -5,7 +5,7 @@ from lists.models import Item
 
 def home_page(request):
     if request.method == "POST":
-        Item.objects.create(text=request.POST("item_text", ""))
+        Item.objects.create(text=request.POST.get("item_text", ""))
         return redirect("/")
     
     items = Item.objects.all()
